cb13536c916f3f38a17b2f6282c6b3442ed99a75
Eduardo Melo <117960142+oeduardomelo@users.noreply.github.com>
2024-11-19 15:01:59 -0300

    Add files via upload

---
+++ projeto/script.js
@@ -0,0 +1,10 @@
+// Suavizar rolagem quando clicar em links do menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
---
+++ script.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({reset: true})
+
+ScrollReveal().reveal('.home', {
+    delay: 250,
+    rotate: {
+        x:100,
+        y: 0,
+        z: 0
+    }
+});
+
+ScrollReveal().reveal('.sobre', {
+    delay: 250,
+    rotate: {
+        x:100,
+        y: 0,
+        z: 0
+    }
+});
+
+ScrollReveal().reveal('.contato', {
+    delay: 250,
+    rotate: {
+        x:100,
+        y: 0,
+        z: 0
+    }
+});
